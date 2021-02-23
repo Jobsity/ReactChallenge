1e65911312e1b5e07e6552235fa8c256b575d120
Cristian Torres Peñaranda <cristian.torres@jobsity.io>
2021-02-23 09:01:16 -0500

    show README.md at index page

---
+++ src/components/Readme.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import Counter from "./Counter";
+
+const README_PATH =
+  "https://raw.githubusercontent.com/Jobsity/ReactChallenge/main/README.md";
+
+function Readme() {
+  const [md, setMd] = useState(null);
+
+  useEffect(() => {
+    fetch(README_PATH, { mode: "cors" })
+      .then((response) => response.text())
+      .then((response) => {
+        setMd(`${response}
+        
+## About Unit Tests:
+The component below has a suite of tests to that could serve as guidance to unit test the calendar functionality, tests are located at \`src/components/Counter.test.jsx\`
+        
+`);
+      });
+  }, []);
+
+  return (
+    <div className="readme">
+      <ReactMarkdown allowDangerousHtml children={md} />
+      {md && <Counter limit={3}/>}
+    </div>
+  );
+}
+
+export default Readme;
